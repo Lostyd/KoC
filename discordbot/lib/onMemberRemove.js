51a4840ae70172fe270026309e681092cdfe26ac
Coochker <khualong@mail.ru>
2019-02-05 00:14:42 +0600

    ssss

--- discordbot/lib/onMemberRemove.js
+++
@@ -1,9 +0,0 @@
-var schems = require('../../db/shems');
-var disuser = schems.disuser;
-
-exports.commands = function (guild) {
-    if(guild.guild.id === '532970439568261142') {
-        disuser.findOneAndDelete({'userid': guild.user.id}, function (err, result) {
-        })
-    }
-};
--- discordbot/lib/onreactionremove.js
+++
@@ -1,7 +0,0 @@
-var Discord = require("discord.js");
-var {Client, RichEmbed, Attachment} = require('discord.js');
-
-
-exports.command = function (emj, user, bot) {
-
-};
